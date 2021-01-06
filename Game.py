eeee73c3f4511f3d6f547facb1282d6a152ad518
flolulu <35403882+flolulu@users.noreply.github.com>
2021-01-06 10:45:36 -0500

    Update Game.py

--- Game.py
+++ Game.py
@@ -3,3 +3,13 @@ import os
 import time
 import random
 
+#
+# let's load the images
+CORONA = pygame.image.load(os.path.join("images", "enemy.png"))
+PLAYER = pygame.image.load(os.path.join("images", "doctor.png"))
+
+# Lasers
+VACCINE = pygame.image.load(os.path.join("images", "bullet.png"))
+
+# Background
+HOSPITAL = pygame.image.load(os.path.join("images", "Hospital_background.jpg"))
